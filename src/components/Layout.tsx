97efd4fb9e921868279233e72235e2f15de727ac
David Koehler <davidekoehler@gmail.com>
2025-06-29 18:37:51 -0500

    fix: remove conflicting container from main layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -92,7 +92,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-grow container mx-auto p-4">
+      <main className="flex-grow">
         {children}
       </main>
       <Footer isContactPage={isContactPage} />
